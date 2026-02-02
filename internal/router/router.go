52ba278fc0705d06565e9bd385465d950759ab13
excflor <andre.ndr31@gmail.com>
2026-02-02 17:09:05 +0700

    feat: configure validator and implement safe type assertions
    - Configured Echo validator using go-playground/validator
    - Implemented safe type assertions for user_id context retrieval in handlers
    - Replaced direct type assertions with safe checks to prevent panics

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,12 +3,23 @@ package router
 import (
 	"go-boilerplate/internal/config"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
 
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
 func NewRouter(cfg *config.Config) *echo.Echo {
 	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(
